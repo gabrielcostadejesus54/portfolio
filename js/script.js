79c921e662088d9e78743fa3b76a985e91e31b58
Gabriel Costa de Jesus <gabrielcostadeveloper@gmail.com>
2025-01-21 18:51:18 -0300

    feat: funcionalidade do menu adicionada

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menu = $('.menu-mobile i')
+let menuItems = $('.menu-mobile ul')
+
+$(menu).click(function(){
+    $(menuItems).slideToggle(function (e){
+        if($(this).css('display') == 'block'){
+            menu.addClass('fa-xmark')
+            menu.removeClass('fa-bars')
+        }else{
+            menu.removeClass('fa-xmark')
+            menu.addClass('fa-bars')
+        }
+    })
+})
